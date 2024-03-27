95e09c4b12052af1e9308825c510a1f098dde407
mashaka38 <161572849+mashaka38@users.noreply.github.com>
2024-03-27 21:54:57 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,33 +1,19 @@
 <?php
-  
-  $receiving_email_address = 'mashakashaibu38@gmail.com';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];  
+    $subject = $_POST["subject"];
+    // Hapa unaweza kufanya chochote na data uliopokea, kama vile kutuma barua pepe au kuokoa kwenye database.
+    
+    // Kwa mfano, unaweza kutuma barua pepe:
+    $to = "bluefaceshaibu@gmail.com";
+    $subject = "Ujumbe kutoka kwenye Fomu ya Tovuti";
+    $body = "Jina: $name\n\nBarua pepe: $email\n\nUjumbe:\n$message";
+    mail($to, $subject, $body);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'something went wrong! "try again later"');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
-  $contact->smtp = array(
-    'host' => 'github.com',
-    'username' => 'mashaka38',
-    'password' => 'blueface38',
-    'port' => '22'
-  );
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    // Unaweza kupeleka mtumiaji kwa ukurasa mwingine baada ya kumaliza usindikaji:
+    header("Location: contact.html");
+    exit;
+}
 ?>
